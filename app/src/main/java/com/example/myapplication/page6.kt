1ee6c04feae5cb37dc8b0391ab29043918075513
mercy34mercy <etukobamasatyan@gmail.com>
2021-03-17 02:12:36 +0900

    alkjds

--- app/src/main/java/com/example/myapplication/page6.kt
+++ app/src/main/java/com/example/myapplication/page6.kt
@@ -3,16 +3,20 @@ package com.example.myapplication
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_page5.*
 import kotlinx.android.synthetic.main.activity_page6.*
+import kotlinx.android.synthetic.main.activity_page8.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import java.net.URLEncoder
 
 class page6 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
