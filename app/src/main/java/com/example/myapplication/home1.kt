50af869e444f9fa4ecff56dac2b7cfb951c3d7ff
yui0805 <hicaruyui@gmail.com>
2021-03-16 16:36:55 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/home1.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class home1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home1)
+
+
+
+
+
+
+    }
+}
