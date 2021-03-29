50e5cb3b33b9a9a3c46022aca279870d10d0ac35
yui0805 <hicaruyui@gmail.com>
2021-03-29 18:05:50 +0900

    mercygayarimasita kowaretaragomennne

---
+++ app/src/main/java/com/example/myapplication/page5_re.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class page5_re : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page5_re)
+    }
+}
