600b55674ebaa697226bdffabcb9f637c452069e
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2021-03-30 14:49:54 +0900

    yabaikamo

--- app/src/main/java/com/example/myapplication/MyViewModel_event.kt
+++ app/src/main/java/com/example/myapplication/MyViewModel_event.kt
@@ -1,3 +1,19 @@
 package com.example.myapplication
 
-data class MyViewModel_event()
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyViewModel_event
+    (
+    var uid2: MutableLiveData<String> = MutableLiveData<String>(""),
+    var date2:MutableLiveData<String> = MutableLiveData<String>(""),
+    var day2:MutableLiveData<String> = MutableLiveData<String>(""),
+    var deadline2:MutableLiveData<String> = MutableLiveData<String>(""),
+    var memo2:MutableLiveData<String> = MutableLiveData<String>(""),
+    var subject2:MutableLiveData<String> = MutableLiveData<String>(""),
+    var title2:MutableLiveData<String> = MutableLiveData<String>(""),
+    var url2:MutableLiveData<String> = MutableLiveData<String>(""),
+    var result2:MutableLiveData<String> = MutableLiveData<String>(""),
+    var id2:MutableLiveData<String> = MutableLiveData<String>("")
+    ) : ViewModel()
+
